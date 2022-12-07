f3e8f07f791f7c9623ec3017484dedd778a39fb2
pokon548 <pokon548@hotmail.com>
2022-12-07 10:10:07 +0800

    Fix for runtime issue

--- pkgs/tencent-qq-electron/default.nix
+++ pkgs/tencent-qq-electron/default.nix
@@ -1,13 +1,13 @@
 { appimageTools, lib, fetchurl, electron, makeWrapper, libsecret }:
 
 let
-  pname = "tencent-qq-electron";
+  pname = "qq";
   version = "2.0.1-429";
   name = "Tencent-QQ-${version}";
 
   src = fetchurl {
     url = "https://dldir1.qq.com/qqfile/qq/QQNT/4691a571/QQ-v${version}_x64.AppImage";
-    sha256 = "sha256-DfNFDiGYTFGetVRlAjpV/cdWcGzRDEGZjR0Dc9aAtXc=";
+    sha256 = "sha256-7izsmUwfEAcQHj6PNcU/cprJRNHj342I62kW316vKo8=";
   };
 
   appimageContents = appimageTools.extract { inherit name src; };
