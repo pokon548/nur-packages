f2236303f84bc073da32adc7f423b23d5444cce0
pokon548 <65808665+pokon548@users.noreply.github.com>
2023-11-15 23:27:07 +0800

    Fix package

--- pkgs/smartdns-rules-adrules/default.nix
+++ pkgs/smartdns-rules-adrules/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://raw.githubusercontent.com/Cats-Team/AdRules/f33359a5f2555c4a36df21e9d25af05b24f4fd08/smart-dns.conf";
-    sha256 = "sha256-1rr79ADVyDJh4zitR29i9xWCMLAj2w3dHiACbztSpdQ=";
+    sha256 = "sha256-8VATGCvCl90GZPtiBBmXxhXHfu852+qqHxve+MOLzlQ=";
   };
 
   unpackPhase = ''
